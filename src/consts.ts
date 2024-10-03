8fb6d22262c5f543130c21f4c9d2e63cdeab3b02
ginger <26461046+gingerchew@users.noreply.github.com>
2024-10-03 11:04:39 -0400

    Adds footer for site

---
+++ src/consts.ts
@@ -0,0 +1,10 @@
+/**
+ * Any sort of sitewide constants should be kept here following the same
+ * naming convention of all caps snakecase
+ * 
+ * e.g.
+ * 
+ * export const YEAR_ESTABLISHED = 2020
+ */
+
+export const SITE_NAME = 'Company Name';
