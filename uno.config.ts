2c34f89cbe4ad9827a7b783c6d70f09d6252e5db
ginger <26461046+gingerchew@users.noreply.github.com>
2024-10-07 11:18:27 -0400

    Final fix for contrast in dark variant sections

--- uno.config.ts
+++ uno.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         'subtitle-small': 'text-4 c-gray-700 leading-[1.3] dark:c-gray-300',
         small: 'text-xs c-gray-700 font-mono leading-[1.2] dark:c-gray-300',
         'project-list': 'grid grid-cols-[1fr_150px] md:grid-cols-[1fr_min-content_150px] lg:grid-cols-[1fr_min-content_min-content_150px] grid-auto-rows-[200px] items-stretch',
-        'section--index': 'inline-grid border-b-1 md:border-l-1 md:border-r-0 border-r-1 border-gray-200 c-gray-700 dark-inside:border-gray-200 dark:border-gray-800 w-10 h-10 text-center place-content-center absolute top-0 left-0 md:translate-x-[-100%]',
+        'section--index': 'inline-grid border-b-1 md:border-l-1 md:border-r-0 border-r-1 border-gray-200 c-gray-700 dark:c-gray-300 dark-inside:border-gray-200 dark:border-gray-800 w-10 h-10 text-center place-content-center absolute top-0 left-0 md:translate-x-[-100%]',
         'section--source': 'c-gray-700 dark:c-gray-300 text-xs font-mono absolute top-[1rem] right-[1rem]',
         'investor--label': 'small pt-10 block pl-12.5 pb-3'
     }],
