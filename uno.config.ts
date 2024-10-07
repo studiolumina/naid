b15948294a8d1ce1c90a5caad9a4d3708aba8649
ginger <26461046+gingerchew@users.noreply.github.com>
2024-10-07 12:36:04 -0400

    Fixes title sizing on certain breakpoints

--- uno.config.ts
+++ uno.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     shortcuts: [{
         wrapper: 'w-100% max-w-[100vw] md:max-w-[calc(100vw-100px)] xl:max-w-[1120px] mx-auto',
         title: 'text-10 leading-[1.5] dark:c-gray-100',
-        'project-title': 'text-8 leading-[1.1]',
+        'project-title': 'text-7 9lg:text-8 leading-[1.1]',
         subtitle: 'text-6 c-gray-700 leading-[1.2] dark:c-gray-300',
         'subtitle-small': 'text-4 c-gray-700 leading-[1.3] dark:c-gray-300',
         small: 'text-xs c-gray-700 font-mono leading-[1.2] dark:c-gray-300',
