4d21f438c96f1126e974ad45135005279a7414b4
ginger <26461046+gingerchew@users.noreply.github.com>
2024-10-07 11:11:56 -0400

    Fixes a contrast issue with the small text on light backgrounds

--- uno.config.ts
+++ uno.config.ts
@@ -6,12 +6,12 @@ export default defineConfig({
         wrapper: 'w-100% max-w-[100vw] md:max-w-[calc(100vw-100px)] xl:max-w-[1120px] mx-auto',
         title: 'text-10 leading-[1.5] dark:c-gray-100',
         'project-title': 'text-8 leading-[1.1]',
-        subtitle: 'text-6 c-gray-600 leading-[1.2] dark:c-gray-300',
-        'subtitle-small': 'text-4 c-gray-600 leading-[1.3] dark:c-gray-300',
-        small: 'text-xs c-gray-600 font-mono leading-[1.2] dark:c-gray-300',
+        subtitle: 'text-6 c-gray-700 leading-[1.2] dark:c-gray-300',
+        'subtitle-small': 'text-4 c-gray-700 leading-[1.3] dark:c-gray-300',
+        small: 'text-xs c-gray-700 font-mono leading-[1.2] dark:c-gray-300',
         'project-list': 'grid grid-cols-[1fr_150px] md:grid-cols-[1fr_min-content_150px] lg:grid-cols-[1fr_min-content_min-content_150px] grid-auto-rows-[200px] items-stretch',
-        'section--index': 'inline-grid border-b-1 md:border-l-1 md:border-r-0 border-r-1 border-gray-200 c-gray-600 dark-inside:border-gray-200 dark:border-gray-800 w-10 h-10 text-center place-content-center absolute top-0 left-0 md:translate-x-[-100%]',
-        'section--source': 'c-gray-600 dark:c-gray-500 text-xs font-mono absolute top-[1rem] right-[1rem]',
+        'section--index': 'inline-grid border-b-1 md:border-l-1 md:border-r-0 border-r-1 border-gray-200 c-gray-700 dark-inside:border-gray-200 dark:border-gray-800 w-10 h-10 text-center place-content-center absolute top-0 left-0 md:translate-x-[-100%]',
+        'section--source': 'c-gray-700 dark:c-gray-500 text-xs font-mono absolute top-[1rem] right-[1rem]',
         'investor--label': 'small pt-10 block pl-12.5 pb-3'
     }],
     variants: [
